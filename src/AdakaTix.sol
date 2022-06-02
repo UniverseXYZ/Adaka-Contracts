9767ba118e62dbdea8450d35844e69898635d887
rshtirmer <rshtirmer@gmail.com>
2022-06-02 12:00:33 -0400

    add biz logic

--- src/AdakaTix.sol
+++ src/AdakaTix.sol
@@ -6,6 +6,11 @@ import {ERC1155} from "solmate/tokens/ERC1155.sol";
 contract AdakaTix is ERC1155 {
 
     string public baseURI;
+    uint256[2] public totalSupply;
+    uint256[2] public maxSupply = [1000, 8000];
+    uint256[2] public price = [0.1 ether, 0.02 ether];
+    uint256 maxMintAmount = 20;
+    mapping(address => uint256) public membershipsMinted;
 
     constructor(string memory _baseURI){
         baseURI = _baseURI;
@@ -14,4 +19,19 @@ contract AdakaTix is ERC1155 {
     function uri(uint256 id) public view virtual override returns (string memory) {
         return string(abi.encodePacked(baseURI, id));
     }
+
+    function mint(uint256 id, uint256 amount) public payable {
+        require(totalSupply[id] + amount <= maxSupply[id], "Over max supply");
+        require(msg.value == amount * price[id], "Value doesn't match price");
+        require(amount < maxMintAmount, "Can't mint that many");
+
+        if (id == 0){
+            require(membershipsMinted[msg.sender] + amount <= 2);
+            membershipsMinted[msg.sender] += amount;
+        }
+
+        totalSupply[id] += amount;
+        _mint(msg.sender, id, amount, "");
+    }
+
 }
